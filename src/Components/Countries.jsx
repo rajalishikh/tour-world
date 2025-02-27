37bbcfef062423f364b34c868f2bb31c6e7a0140
rajalishikhe <nahidujjamanraj@gmail.com>
2025-02-27 21:44:43 +0600

    I am create a list where we can see how many country I am visited

--- src/Components/Countries.jsx
+++ src/Components/Countries.jsx
@@ -6,6 +6,12 @@ import Country from "./Country";
 
 const Countries = () => {
     const [countries,SetCountries]=useState([])
+    const [countries_name,setCountriesName]=useState([])
+    const handleVisited=(data)=>{
+      
+      const new_Array=[...countries_name,data];
+      setCountriesName(new_Array)
+    }
 
   useEffect(()=>{
     async function load_country_data() {
@@ -16,16 +22,19 @@ const Countries = () => {
     }
     // call the function
     load_country_data()
-
-
-  },[])
+},[])
     return (
         <div >
             <h1>My All Countries</h1>
             <p>Length {countries.length}</p>
+            <p>My country visited name:</p>
+            <ul>
+              {countries_name.map((country_name_single)=><li key={country_name_single.cca3}>{country_name_single.name.common}</li>)}
+             </ul>
            <div className="country_container">
            {
-                countries.map((item)=><Country key={item.cca3} country_details={item}></Country>)
+                countries.map((item)=><Country key={item.cca3} country_details={item}
+                 import_handleVisited={handleVisited} ></Country>)
             }
            </div>
             
--- src/Components/Country.jsx
+++ src/Components/Country.jsx
@@ -2,8 +2,8 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 import "../App.css";
 
-const Country = ({country_details}) => {
-    console.log(country_details)
+const Country = ({country_details,import_handleVisited}) => {
+    
     const {name,flags}=country_details
     const [visited,setVisited]=useState(false)
     function handleVisited(){
@@ -11,10 +11,11 @@ const Country = ({country_details}) => {
     }
     return (
         
-        <div className="box">
-        <p>Name: {name.common}</p>
+        <div className={`box ${visited?"box-background":"Nothing"}`}>
+        <p style={{color:visited?"yellow":"white"}}>Name: {name.common}</p>
         <img src={flags.png} alt="Country Flags Name" width="200px" /><br />
         <button onClick={handleVisited}>{visited?"Going":"Visited"}</button>
+        <button onClick={()=>import_handleVisited(country_details)}>Mark Visited</button>
         <p>{visited?"The country visiting is complete":"I am not gone this country any Time"}</p>
         
     </div>
@@ -25,5 +26,6 @@ const Country = ({country_details}) => {
 export default Country;
 // default p[rop types
 Country.propTypes={
-    country_details:PropTypes.object.isRequired
+    country_details:PropTypes.object.isRequired,
+    import_handleVisited:PropTypes.func.isRequired
 }
